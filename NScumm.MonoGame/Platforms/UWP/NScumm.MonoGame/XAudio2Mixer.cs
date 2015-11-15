c23a75b5ad659a8d5f26d3a6bb26f681675b938b
scemino <scemino74@gmail.com>
2015-11-15 11:10:23 +0100

    Improve XAudio2Mixer

--- NScumm.MonoGame/Platforms/UWP/NScumm.MonoGame/XAudio2Mixer.cs
+++ NScumm.MonoGame/Platforms/UWP/NScumm.MonoGame/XAudio2Mixer.cs
@@ -1,31 +1,74 @@
 ﻿using NScumm.Core.Audio;
 using NScumm.Core.Audio.SampleProviders;
 using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using SharpDX.XAudio2;
 using SharpDX.Multimedia;
 using SharpDX;
-using System.Threading;
 
 namespace NScumm.MonoGame
 {
     class XAudio2Mixer : IAudioOutput
     {
-        private readonly Timer _timer;
+        class XAudioBuffer: IDisposable
+        {
+            private readonly byte[] _samples;
+            private readonly DataStream _dataStream;
+
+            public AudioBuffer Buffer { get; }
+
+            public XAudioBuffer(int size)
+            {
+                _samples = new byte[size];
+                _dataStream = DataStream.Create(_samples, true, true);
+                Buffer = new AudioBuffer
+                {
+                    Stream = _dataStream,
+                    AudioBytes = (int)_dataStream.Length,
+                    Flags = BufferFlags.EndOfStream
+                };
+            }
+
+            public void Dispose()
+            {
+                _dataStream.Dispose();
+            }
+
+            public void FillWith(IAudioSampleProvider audioSampleProvider)
+            {
+                Array.Clear(_samples, 0, _samples.Length);
+                audioSampleProvider.Read(_samples, _samples.Length);
+                _dataStream.Position = 0;
+                _dataStream.Write(_samples, 0, _samples.Length);
+            }
+        }
+        private const int NumBuffers = 8;
+        private const int BufferSize = 4096 * 2 * 2;
+
         private IAudioSampleProvider _audioSampleProvider;
         private XAudio2 _xAudio;
         private SourceVoice _voice;
-        private byte[] _samples;
-        private DataStream _dataStream;
-        private AudioBuffer _buffer;
-        private object _gate = new object();
-        private MasteringVoice _masteringVoice;
+
+        private XAudioBuffer[] _buffers;
+
+        private readonly MasteringVoice _masteringVoice;
+        private int _index;
+        private Task _audioThread;
+        private bool _quit;
 
         public XAudio2Mixer()
         {
             _xAudio = new XAudio2();
             _xAudio.StartEngine();
             _masteringVoice = new MasteringVoice(_xAudio);
-            _timer = new Timer(OnTimer, null, Timeout.Infinite, Timeout.Infinite);
+            _buffers = new XAudioBuffer[NumBuffers];
+
+            for (var i = 0; i < NumBuffers; i++)
+            {
+                _buffers[i] = new XAudioBuffer(BufferSize);
+            }
         }
 
         public void Play()
@@ -34,35 +77,35 @@ namespace NScumm.MonoGame
             {
                 _voice.Start();
             }
-            _timer.Change(TimeSpan.Zero, TimeSpan.FromSeconds(1.0));
         }
 
         public void Pause()
         {
-            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+            Stop();
         }
 
         public void SetSampleProvider(IAudioSampleProvider audioSampleProvider)
         {
             var format = ToWaveFormat(audioSampleProvider.AudioFormat);
             _voice = new SourceVoice(_xAudio, format, true);
-            _samples = new byte[audioSampleProvider.AudioFormat.AverageBytesPerSecond];
-            _dataStream = DataStream.Create(_samples, true, true);
-            _buffer = new AudioBuffer
-            {
-                Stream = _dataStream,
-                AudioBytes = (int)_dataStream.Length,
-            };
             _audioSampleProvider = audioSampleProvider;
-            FillBuffer();
+            for (var i = 0; i < NumBuffers; i++)
+            {
+                FillBuffer(i);
+            }
+            _audioThread = Task.Factory.StartNew(OnAudioThread, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default);
             _voice.Start();
         }
 
         public void Dispose()
         {
+            _quit = true;
+            _audioThread.Wait();
             Stop();
-            _timer.Dispose();
-            _dataStream.Dispose();
+            foreach (var buffer in _buffers)
+            {
+                buffer.Dispose();
+            }
             _voice.Dispose();
             _masteringVoice.Dispose();
             _xAudio.Dispose();
@@ -70,36 +113,35 @@ namespace NScumm.MonoGame
 
         public void Stop()
         {
-            _timer.Change(Timeout.Infinite, Timeout.Infinite);
-            _voice.Stop();
+            if (_voice != null)
+            {
+                _voice.Stop();
+            }
+        }
+
+        private void OnAudioThread()
+        {
+            while (!_quit)
+            {
+                var state = _voice.State;
+                while (state.BuffersQueued < NumBuffers)
+                {
+                    FillBuffer(_index);
+                    _index = ++_index % NumBuffers;
+                    state = _voice.State;
+                }
+            }
+        }
+
+        private void FillBuffer(int index)
+        {
+            _buffers[index].FillWith(_audioSampleProvider);
+            _voice.SubmitSourceBuffer(_buffers[index].Buffer, null);
         }
 
         private static WaveFormat ToWaveFormat(AudioFormat format)
         {
             return new WaveFormat(format.SampleRate, format.Channels);
-        }
-
-        private void OnTimer(object state)
-        {
-            if (_audioSampleProvider != null)
-            {
-                FillBuffer();
-            }
-        }
-
-        private void FillBuffer()
-        {
-            lock (_gate)
-            {
-                Array.Clear(_samples, 0, _samples.Length);
-                var count = _audioSampleProvider.Read(_samples, _samples.Length);
-                if (count > 0)
-                {
-                    _dataStream.Position = 0;
-                    _dataStream.Write(_samples, 0, count);
-                }
-            }
-            _voice.SubmitSourceBuffer(_buffer, null);
         }
     }
 }
